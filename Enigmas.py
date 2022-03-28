4dca5fbc5abdfa4abcd0f7d789947b03b51673d6
Denis <96110531+De0nis@users.noreply.github.com>
2022-03-28 18:57:49 +0700

    Update Enigmas.py

--- Enigmas.py
+++ Enigmas.py
@@ -1,4 +1,5 @@
 """
+Spy-Play-Game)))))
 This program encodes messages for transmission in an open way.the checksum and the sender's mac 
 are used. you need to create a document with a message and a document for encoding 2 files .txt
 PyQt5 need
